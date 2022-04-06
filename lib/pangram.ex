184f768bb65e448ecca90d1c936c711e9f3f6e74
Kerry Buckley <kerryjbuckley@gmail.com>
2022-04-06 16:30:21 +0100

    Delete boilerplate

--- lib/pangram.ex
+++ lib/pangram.ex
@@ -1,18 +1,2 @@
 defmodule Pangram do
-  @moduledoc """
-  Documentation for `Pangram`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Pangram.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/pangram_test.exs
+++ test/pangram_test.exs
@@ -1,8 +1,3 @@
 defmodule PangramTest do
   use ExUnit.Case
-  doctest Pangram
-
-  test "greets the world" do
-    assert Pangram.hello() == :world
-  end
 end
