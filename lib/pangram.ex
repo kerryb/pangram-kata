83346dfed612b435ffe1a17f6780fdab69392a28
Kerry Buckley <kerryjbuckley@gmail.com>
2022-04-07 15:31:23 +0100

    First attempt, with sets and an attribute/constant

--- lib/pangram.ex
+++ lib/pangram.ex
@@ -1,2 +1,13 @@
 defmodule Pangram do
+  defmodule Helper do
+    def letters(string) do
+      string |> String.downcase() |> String.codepoints() |> MapSet.new()
+    end
+  end
+
+  @alphabet Helper.letters("abcdefghijklmnopqrstuvwxyz")
+
+  def pangram?(sentence) do
+    MapSet.subset?(@alphabet, Helper.letters(sentence))
+  end
 end
--- test/pangram_test.exs
+++ test/pangram_test.exs
@@ -1,3 +1,25 @@
 defmodule PangramTest do
   use ExUnit.Case
+
+  describe "Pangram.pangram?/1" do
+    test "returns true if all letters of the alphabet are present" do
+      assert Pangram.pangram?("qwertyuiopasdfghjklzxcvbnm")
+    end
+
+    test "returns false if not all letters of the alphabet are present" do
+      refute Pangram.pangram?("qwertyuiopasdfghjklzxcvbn")
+    end
+
+    test "allows extra letters" do
+      assert Pangram.pangram?("aaabbbbccdefghijklmnopqrstuvwxyz")
+    end
+
+    test "ignores non-alpha characters" do
+      assert Pangram.pangram?("the quick brown fox jumps over 42 lazy dogs!")
+    end
+
+    test "ignores case" do
+      assert Pangram.pangram?("The Quick Brown Fox Jumps Over a Lazy Dog")
+    end
+  end
 end
